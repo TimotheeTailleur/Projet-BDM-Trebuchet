3d3cab7cb734fee3b007cbecb8fe66f3c4a79562
TimotheeTailleur <tailleur@.timothee@telecom-st-etienne.fr>
2017-04-26 20:37:50 +0200

    Ajout d'une classe myglwidget pour affichage (pas encore intégrée à la fenêtre principale)

---
+++ Projet_Trebuchet/myglwidget.cpp
@@ -0,0 +1,12 @@
+#include "myglwidget.h"
+
+#include <QtWidgets>
+#include <QtOpenGL>
+#include <GL/glu.h>
+
+#include "myglwidget.h"
+
+MyGLWidget::MyGLWidget()
+{
+
+}
---
+++ Projet_Trebuchet/myglwidget.h
@@ -0,0 +1,31 @@
+// myglwidget.h
+
+#ifndef MYGLWIDGET_H
+#define MYGLWIDGET_H
+
+#include <QGLWidget>
+
+
+class MyGLWidget : public QGLWidget
+{
+    Q_OBJECT
+public:
+    MyGLWidget();
+signals:
+
+public slots:
+
+protected:
+
+
+public slots:
+
+signals:
+
+
+private:
+
+};
+
+#endif // MYGLWIDGET_H
+
